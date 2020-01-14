b90a2e14de8ae33fde7b24a0ad601d28bcc099c2
ra-211 <59528812+ra-211@users.noreply.github.com>
2020-01-14 21:15:57 +0200

    routes

---
+++ users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = require('express-promise-router')();
+
+
+const UsersController = require(../controllers/users');
+
+router.route('/singup')
+   .post(UsersController.singup);
+
+router.route('/singin')
+   .post(UsersController.singin);
+
+router.route('/home')
+   .get(UsersController.home);
+
+
+
+
+module.exports = router;
