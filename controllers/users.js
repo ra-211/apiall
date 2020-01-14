54ad6c674dc352be98433ac936c51a1485f89280
ra-211 <59528812+ra-211@users.noreply.github.com>
2020-01-14 21:23:24 +0200

    Create users.js

---
+++ controllers/users.js
@@ -0,0 +1,26 @@
+module.exports = {
+  singup: async (req, res, next)=> {
+
+      console.log('UserController.singup() called');
+
+   }
+
+   singin: async (req, res, next)=> {
+
+      console.log('UserController.singin() called');
+
+   }
+
+
+
+   home: async (req, res, next)=> {
+
+      console.log('UserController.home() called');
+
+   }
+
+}
+
+
+
+
